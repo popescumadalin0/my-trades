be53a92d254fd0255b06f10fc1b6e4d78107ffb1
madalinpopescu <dizzyforester@gmail.com>
2026-02-19 20:44:09 +0200

    .

--- server/src/MyTrades.Domain/DependencyInjection.cs
+++ server/src/MyTrades.Domain/DependencyInjection.cs
@@ -1,4 +1,3 @@
-using Marten;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,27 +5,7 @@ namespace MyTrades.Domain;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDomainServices(this IServiceCollection services, IConfiguration config)
-    {
-        services.AddMarten(options =>
-            {
-                // Establish the connection string to your Marten database
-                options.Connection(config.GetConnectionString("Marten")!);
-
-                // If you want the Marten controlled PostgreSQL objects
-                // in a different schema other than "public"
-                options.DatabaseSchemaName = "other";
-
-                // There are of course, plenty of other options...
-            })
-
-// This is recommended in new development projects
-            .UseLightweightSessions()
-
-// If you're using Aspire, use this option *instead* of specifying a connection
-// string to Marten
-            .UseNpgsqlDataSource();
-
+    public static IServiceCollection AddDomainServices(this IServiceCollection services, IConfiguration config){
         return services;
     }
 }
